6396ccaec60fdb9da6daca44ee662b28e58c06c9
Egga <57639719+eggstrema@users.noreply.github.com>
2022-10-11 00:07:32 +0200

    organize imports

--- src/test/java/de/egga/farmerschoice/ToonReaderTest.java
+++ src/test/java/de/egga/farmerschoice/ToonReaderTest.java
@@ -1,18 +1,12 @@
 package de.egga.farmerschoice;
 
 import de.egga.farmerschoice.toon.Toon;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.util.List;
-
-import static java.nio.file.Path.of;
+import de.egga.farmerschoice.toon.ToonReader;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.List;
-import de.egga.farmerschoice.toon.ToonReader;
 
 import static java.nio.file.Path.of;
 import static org.assertj.core.api.Assertions.assertThat;
