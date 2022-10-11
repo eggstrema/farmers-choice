65e6746f59752dae4277fef53de38110f3a97f71
Egga <57639719+eggstrema@users.noreply.github.com>
2022-10-11 11:31:52 +0200

    use service

--- src/main/java/de/egga/farmerschoice/Controller.java
+++ src/main/java/de/egga/farmerschoice/Controller.java
@@ -1,8 +1,10 @@
 package de.egga.farmerschoice;
 
+import de.egga.farmerschoice.characters.CharacterService;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -12,10 +14,22 @@ import java.io.IOException;
 @RestController
 public class Controller {
 
+    @Autowired
+    CharacterService characterService;
+
+    @GetMapping("/phoenix")
+    public String phoenix() throws IOException {
+
+        return characterService.getMyPhoenixCharacters();
+    }
+
     @GetMapping("/toons")
     public String characters() throws IOException {
-        OkHttpClient client = new OkHttpClient();
+        if (true) {
+            throw new RuntimeException("forbidden for now");
+        }
 
+        OkHttpClient client = new OkHttpClient();
 
         String url = "https://swgoh.gg/api/characters/?format=json";
         Request request = new Request.Builder().url(url).build();
@@ -27,8 +41,11 @@ public class Controller {
 
     @GetMapping("/gear")
     public String gear() throws IOException {
-        OkHttpClient client = new OkHttpClient();
+        if (true) {
+            throw new RuntimeException("forbidden for now");
+        }
 
+        OkHttpClient client = new OkHttpClient();
 
         String url = "https://swgoh.gg/api/gear/126/?format=json";
         Request request = new Request.Builder().url(url).build();
